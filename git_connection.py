f8f47129cf3ca54f936aabecbd9afa8e6cce7b1e
Andrii Levytskyi <alevyts@softserveinc.com>
2019-03-05 13:58:33 +0200

    Add project structure

---
+++ git_connection.py
@@ -0,0 +1,9 @@
+"""Module provides connection to github.
+
+:Author: alevyts.
+"""
+
+
+def login_to_github(ssh_url=None, https_url=None, ssh_key_path=None,
+                    username=None, password=None):
+    pass
